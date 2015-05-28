05d16861069f6740c4012e2c09dfb5a4457cf00b
Matthew Filmer <mattfilmer@gmail.com>
2015-05-28 12:49:09 -0400

    Major changes. Removed sandbox

--- main.hs
+++ main.hs
@@ -1,1 +1,15 @@
-main = do putStrLn "test"
+{-# LANGUAGE OverloadedStrings #-}
+
+import Codec.Picture
+
+imageCreator :: String -> IO ()
+imageCreator path = writePng path $ generateImage pixelRenderer 250 300
+   where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 128
+
+loadSampleImage path = case decodePng path of
+  Left errorMsg -> putStrLn errorMsg
+  Right sampleImage -> putStrLn "Loaded Successfully"
+
+main = do 
+  imageCreator "test.png"
+  loadSampleImage "test.png"
