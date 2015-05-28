5bf7f15db79084b0637297dd87507fc64dcf0366
Matthew Filmer <mattfilmer@gmail.com>
2015-05-28 14:00:23 -0400

    Finished

--- main.hs
+++ main.hs
@@ -1,15 +1,50 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Codec.Picture
+import Codec.Picture.RGBA8 (fromDynamicImage)
+import System.Environment (getArgs)
 
 imageCreator :: String -> IO ()
 imageCreator path = writePng path $ generateImage pixelRenderer 250 300
    where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 128
 
-loadSampleImage path = case decodePng path of
-  Left errorMsg -> putStrLn errorMsg
-  Right sampleImage -> putStrLn "Loaded Successfully"
+loadSampleImage :: String -> IO()
+loadSampleImage path = do
+  img <- readPng path
+  case img of
+    Left errorMsg -> putStrLn errorMsg
+    Right sampleImage -> putStrLn "Loaded Successfully"
+
+insertSpaces :: Image PixelRGBA8 -> Int -> Image PixelRGBA8
+insertSpaces image spaceHeight = generateImage (imageSpacer image spaceHeight) width height
+  where
+    width = imageWidth image
+    height = imageHeight image * 2 - spaceHeight
+
+imageSpacer :: Image PixelRGBA8 -> Int -> Int -> Int -> PixelRGBA8
+imageSpacer baseImage spaceHeight x y
+  | ifFromRaw = pixelAt baseImage x (y `div` 2)
+  | otherwise = PixelRGBA8 0 0 0 0
+  where
+    ifFromRaw = y `div` spaceHeight `mod` 2 == 0
+    realY = (y `div` (2 * spaceHeight)) * spaceHeight + y `mod` spaceHeight
+
+--insertSpaces :: Int -> Int -> PixelRGBA8
+--insertSpaces x y = PixelRGBA8 g g b a
 
 main = do 
   imageCreator "test.png"
-  loadSampleImage "test.png"
+  args <- getArgs
+  let inName = args !! 0
+  let spaceHeight = read $ args !! 1
+  let outName = inName ++ "-spaced.png"
+  
+  rawImage <- readPng (head args)
+  case rawImage of
+    Left errorMsg -> putStrLn errorMsg
+    Right rawImage -> do
+      putStrLn "Image Loaded..."
+      let spacedImg = insertSpaces (fromDynamicImage rawImage) spaceHeight
+      writePng outName spacedImg
+      putStrLn "Spaces Added"
+      
